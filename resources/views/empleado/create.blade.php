d4e29dae23667c4f0f68499271ae2195e4d8910e
JGall3go <hf2264185@gmail.com>
2022-06-19 20:05:58 -0500

    v1.0
    Creacion de empleados

---
+++ resources/views/empleado/create.blade.php
@@ -0,0 +1,6 @@
+<form action="" method="POST" enctype="multipart/form-data">
+    <input type="text" name="" id="">
+    <input type="text" name="" id="">
+    <input type="text" name="" id="">
+    <input type="text" name="" id="">
+</form>
